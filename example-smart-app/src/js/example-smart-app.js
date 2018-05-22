d26231825232094ea9316b102f7279bcb4560a9f
linduff <calinduff@gmail.com>
2018-05-22 11:42:16 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -144,7 +144,7 @@
 
   window.drawVisualization = function(p) {
     p.gender = "female";
-    console.log(p);
+    console.log(p.resourceType);
     //smart.api.update({type: p.resourceType, data: JSON.stringify(p), id: p.id});
     $('#holder').show();
     $('#loading').hide();
